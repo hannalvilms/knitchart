308b448edc304cf52d64d076500b9d0186410f10
hannalvilms <hannaliisavilms@gmail.com>
2022-02-22 13:15:46 +0200

    Added footer

---
+++ src/comps/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="container footer">
+        <h6>Hanna-Liisa Vilms © Knitchart 2022</h6>
+        <p>Free charts found from Pinterest</p>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ImgGrid from '../comps/ImgGrid';
+import React from "react";
+import ImgGrid from "../comps/ImgGrid";
+import Footer from "../comps/Footer";
 
 function Home() {
-  return <div className='container'>
-      <div className='row'>
-      <div>
-          <ImgGrid title='Free Charts'/>
+  return (
+    <div className="container">
+      <div className="row">
+        <div>
+          <ImgGrid title="Free Charts" />
         </div>
       </div>
-  </div>;
+      <Footer/>
+    </div>
+  );
 }
 export default Home;
